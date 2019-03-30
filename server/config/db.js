7f4d1564cd665d9f21ceedd4e580de1502efc56c
sgohary <syednacer@gmail.com>
2019-03-30 15:03:35 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+   //"URI" : "mongodb://localhost/test",
+    //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+    "URI": "mongodb+srv://thomas:a123456@cluster0-hrrta.azure.mongodb.net/test?retryWrites=true",
+
+    "secret": "SomeSecret"
+}
